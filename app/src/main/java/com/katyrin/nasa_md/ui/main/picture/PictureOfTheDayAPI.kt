fe0d9830e3a127fd8cdbafb446ee8b2becb58634
Roman <katyrinroman@mail.ru>
2021-04-18 17:33:45 +0700

    add bottom_sheet_layout

--- app/src/main/java/com/katyrin/nasa_md/ui/main/picture/PictureOfTheDayAPI.kt
+++ app/src/main/java/com/katyrin/nasa_md/ui/main/picture/PictureOfTheDayAPI.kt
@@ -1,6 +1,5 @@
 package com.katyrin.nasa_md.ui.main.picture
 
-import com.katyrin.nasa_md.ui.main.picture.PODServerResponseData
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Query
--- app/src/main/java/com/katyrin/nasa_md/ui/main/picture/PictureOfTheDayFragment.kt
+++ app/src/main/java/com/katyrin/nasa_md/ui/main/picture/PictureOfTheDayFragment.kt
@@ -7,9 +7,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import coil.load
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.katyrin.nasa_md.R
 import com.katyrin.nasa_md.databinding.MainFragmentBinding
 
@@ -21,6 +23,7 @@ class PictureOfTheDayFragment : Fragment() {
         private var isMain = true
     }
 
+    private lateinit var bottomSheetBehavior: BottomSheetBehavior<ConstraintLayout>
     private lateinit var binding: MainFragmentBinding
     private val viewModel: PictureOfTheDayViewModel by lazy {
         ViewModelProvider(this).get(PictureOfTheDayViewModel::class.java)
@@ -34,6 +37,7 @@ class PictureOfTheDayFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setBottomSheetBehavior(binding.includeLayout.bottomSheetContainer)
 
         binding.inputLayout.setEndIconOnClickListener {
             startActivity(Intent(Intent.ACTION_VIEW).apply {
@@ -80,6 +84,29 @@ class PictureOfTheDayFragment : Fragment() {
         Toast.makeText(context, string, Toast.LENGTH_SHORT).apply {
             show()
         }
+    }
+
+    private fun setBottomSheetBehavior(bottomSheet: ConstraintLayout) {
+        bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet)
+        bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
+
+        bottomSheetBehavior.addBottomSheetCallback(object : BottomSheetBehavior.BottomSheetCallback() {
+            override fun onStateChanged(bottomSheet: View, newState: Int) {
+                when(newState) {
+                    BottomSheetBehavior.STATE_DRAGGING -> toast("STATE_DRAGGING")
+                    BottomSheetBehavior.STATE_COLLAPSED -> toast("STATE_COLLAPSED")
+                    BottomSheetBehavior.STATE_EXPANDED -> toast("STATE_EXPANDED")
+                    BottomSheetBehavior.STATE_HALF_EXPANDED -> toast("STATE_HALF_EXPANDED")
+                    BottomSheetBehavior.STATE_HIDDEN -> toast("STATE_HIDDEN")
+                    BottomSheetBehavior.STATE_SETTLING -> toast("STATE_SETTLING")
+                }
+            }
+
+            override fun onSlide(bottomSheet: View, slideOffset: Float) {
+                toast("onSlide")
+            }
+
+        })
     }
 
 }
